cf753437f1e195d22e43bb0466b705d409ceb5ee
piyush-3d <assudani66@gmail.com>
2024-11-02 23:19:00 +0530

    fix:Remove extra comments

--- main.js
+++ main.js
@@ -26,20 +26,13 @@ const rgbeLoader = new RGBELoader();
 rgbeLoader.load('/hdr/Collection_filtered.hdr', (texture) => {
   texture.flipY = false,
   texture.channel = 1
-  // No need for mapping since we're using it as a lightmap
-
-  // Load the GLB model and apply the HDR as a lightmap
   const gltfLoader = new GLTFLoader();
   gltfLoader.load('/tes-light-mapper.glb', (gltf) => {
     const model = gltf.scene;
-
-
-    // Traverse each mesh in the model and apply the HDR lightmap to the second UV channel
     model.traverse((child) => {
       if (child.isMesh) {
-
-          child.material.lightMap = texture; // Assign the HDR texture directly
-          child.material.lightMapIntensity = 1.0; // Adjust intensity as needed
+          child.material.lightMap = texture; 
+          child.material.lightMapIntensity = 1.0;
       }
     });
 
